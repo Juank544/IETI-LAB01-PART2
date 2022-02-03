e96331659320541be25d317254ce02c635145722
Juank544 <camilo.gv@live.com>
2022-02-02 22:22:20 -0500

    Task Data and Dto

---
+++ src/main/java/co/edu/escuelaing/Task/dto/TaskDto.java
@@ -0,0 +1,49 @@
+package co.edu.escuelaing.Task.dto;
+
+public class TaskDto {
+    private String name;
+    private String description;
+    private String status;
+    private String assignedTo;
+    private String dueDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getAssignedTo() {
+        return assignedTo;
+    }
+
+    public void setAssignedTo(String assignedTo) {
+        this.assignedTo = assignedTo;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+}
